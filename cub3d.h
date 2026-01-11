df7575d245e2974b447c47fb5356ace80d340064
root <root@DESKTOP-7CAO994.localdomain>
2026-01-11 10:31:41 +0100

    cub3d.h norminetteado

--- cub3d.h
+++ cub3d.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sbolivar <sbolivar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 10:17:13 by root              #+#    #+#             */
-/*   Updated: 2026/01/09 14:59:39 by sbolivar         ###   ########.fr       */
+/*   Updated: 2026/01/11 10:31:07 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,11 +35,11 @@
 /* In cub3d.h - Already good, but ensure t_texture has correct fields */
 typedef struct s_texture
 {
-    mlx_texture_t *texture;
-    uint32_t *pixels;  // RGBA format
-    int width;
-    int height;
-} t_texture;
+	mlx_texture_t	*texture;
+	uint32_t		*pixels;//  RGBA format
+	int				width;
+	int				height;
+}	t_texture;
 
 typedef struct s_player
 {
@@ -55,35 +55,33 @@ typedef struct s_player
 
 typedef struct s_ray
 {
-	float dir_x;
-	float dir_y;
-	float delta_x;
-	float delta_y;
-	float side_x;
-	float side_y;
-	int   step_x;
-	int   step_y;
-	int   map_x;
-	int   map_y;
-	int   side;
-	float perp_dist;
-	float wall_x;
-	int   draw_start;
-	int   draw_end;
-	int   wall_height;
-	int   tex_x;
-	float tex_pos;
-	float tex_step;
+	float		dir_x;
+	float		dir_y;
+	float		delta_x;
+	float		delta_y;
+	float		side_x;
+	float		side_y;
+	int			step_x;
+	int			step_y;
+	int			map_x;
+	int			map_y;
+	int			side;
+	float		perp_dist;
+	float		wall_x;
+	int			draw_start;
+	int			draw_end;
+	int			wall_height;
+	int			tex_x;
+	float		tex_pos;
+	float		tex_step;
 }	t_ray;
-
 
 typedef struct s_game
 {
-	
-	t_texture   north;
-    t_texture   south;
-    t_texture   east;
-    t_texture   west;
+	t_texture	north;
+	t_texture	south;
+	t_texture	east;
+	t_texture	west;
 
 	mlx_t		*mlx;
 	mlx_image_t	*image;
@@ -108,51 +106,48 @@ typedef struct s_game
 	uint32_t	floor_color;
 }	t_game;
 
-
 //			Rays! 🍊
-void	calc_ray_dir(t_ray *r, int ray_id, int num_rays, t_player *p);
-void	init_dda(t_ray *r, t_player *p);
+void		calc_ray_dir(t_ray *r, int ray_id, int num_rays, t_player *p);
+void		init_dda(t_ray *r, t_player *p);
 t_texture	*select_wall_texture(t_game *g, t_ray *r);
-void	calc_wall_params(t_ray *r, t_player *p, t_game *g, t_texture **tex);
-void	draw_ray_column(t_game *g, t_ray *r, t_texture *tex, int x);
+void		calc_wall_params(t_ray *r, t_player *p, t_game *g, t_texture **tex);
+void		draw_ray_column(t_game *g, t_ray *r, t_texture *tex, int x);
 
-//
-void	get_text(int fd, char **str, t_game *game, int i);
-char	*give_map_line(char *map, char *temp);
-void	free_game(t_game *game);
-void	ft_my_hook(mlx_key_data_t keydata, void	*param);
-bool 	touch(t_game *game, float px, float py);
-int		player_case(int x, int y, char **map);
-int		zero_case(int x, int y, char **map);
-int 	parse_fc_line(const char *line, uint32_t *out_color);
-void	get_paths(t_game *game);
-void	special_strlcpy(char *dst, char *src, size_t size, size_t j);
-int		wall_case(int x, int y, char **map);
-int		space_case(int x, int y, char **map);
-void	init_player(t_player *player);
-void	init_vars(t_game *game, t_player *player);
-void	map_drawer(t_game *game);
-int		ft_strlen_strings(char **av);
-char	**get_map(t_game *game, int y, int player);
-int		map_parse(t_game *game, int i, int y);
-int		check_cases(int	*x, int y, char **map, t_player *player);
-int		check_walls(char **map, t_player *player);
-void	init_game(t_game *game);
-void	pixel_placer(int x, int y, uint32_t color, t_game *game);
-// bool	touch(t_game *game, int player_x, int player_y);
-void	draw_minimap(t_game *g, int py, int px, int mx);
-void	draw_cleaner(t_game *game);
-void	init_player(t_player *player);
-void	pixel_placer(int x, int y, uint32_t color, t_game *game);
-void	map_drawer(t_game *game);
-void	init_game(t_game *game);
-void	draw_square_1(int x, int y, uint32_t color, t_game *game);
-float	distance(float x, float y);
-void	draw_square_2(int x, int y, uint32_t color, t_game *game);
-void	draw_square_3(int x, int y, uint32_t color, t_game *game);
-void	load_all_textures(t_game *game);
-void	init_dda_utils(t_ray *r, t_player *p, float pos_x, float pos_y);
-
-int	ft_validate_file(char	*map_path);
+void		get_text(int fd, char **str, t_game *game, int i);
+char		*give_map_line(char *map, char *temp);
+void		free_game(t_game *game);
+void		ft_my_hook(mlx_key_data_t keydata, void	*param);
+bool		touch(t_game *game, float px, float py);
+int			player_case(int x, int y, char **map);
+int			zero_case(int x, int y, char **map);
+int			parse_fc_line(const char *line, uint32_t *out_color);
+void		get_paths(t_game *game);
+void		special_strlcpy(char *dst, char *src, size_t size, size_t j);
+int			wall_case(int x, int y, char **map);
+int			space_case(int x, int y, char **map);
+void		init_player(t_player *player);
+void		init_vars(t_game *game, t_player *player);
+void		map_drawer(t_game *game);
+int			ft_strlen_strings(char **av);
+char		**get_map(t_game *game, int y, int player);
+int			map_parse(t_game *game, int i, int y);
+int			check_cases(int	*x, int y, char **map, t_player *player);
+int			check_walls(char **map, t_player *player);
+void		init_game(t_game *game);
+void		pixel_placer(int x, int y, uint32_t color, t_game *game);
+// bool		touch(t_game *game, int player_x, int player_y);
+void		draw_minimap(t_game *g, int py, int px, int mx);
+void		draw_cleaner(t_game *game);
+void		init_player(t_player *player);
+void		pixel_placer(int x, int y, uint32_t color, t_game *game);
+void		map_drawer(t_game *game);
+void		init_game(t_game *game);
+void		draw_square_1(int x, int y, uint32_t color, t_game *game);
+float		distance(float x, float y);
+void		draw_square_2(int x, int y, uint32_t color, t_game *game);
+void		draw_square_3(int x, int y, uint32_t color, t_game *game);
+void		load_all_textures(t_game *game);
+void		init_dda_utils(t_ray *r, t_player *p, float pos_x, float pos_y);
+int			ft_validate_file(char	*map_path);
 
 #endif
--- src/free.c
+++ src/free.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   free.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sbolivar <sbolivar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/18 14:40:30 by sbolivar          #+#    #+#             */
-/*   Updated: 2026/01/09 12:39:34 by sbolivar         ###   ########.fr       */
+/*   Updated: 2026/01/11 10:19:55 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,16 +28,6 @@ void	free_double(char **str)
 
 void	free_game(t_game *game) // trabajar en esta funcion
 {
-	// if (game->image)
-	// 	mlx_delete_image(game->mlx, game->image);
-	// free(game->mlx);
-	// free(game);
-	// free(game->select_map);
-	// free(game->pixel);
-	// free(game->title);
-	// free_double(game->map);
-	// free_double(game->inf);
-	// // free_double(game->paths);
 	mlx_terminate(game->mlx);
 	free(game);
 }
